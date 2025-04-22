a214cb45b0036ffac19ad5a55e8b17808adcc7fb
Francisco <francisco.albrechtribas@gmail.com>
2025-04-22 18:06:27 -0300

    Adicionado o folder de requests a API e utilizado(para teste) na página Exercicios. Necessário criar src/private/token.js para guardar token

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Footer = () => {
   return (
     <footer className="footer bg-gray-900 text-white p-6 mt-8">
--- my-app/src/pages/Exercicios.js
+++ my-app/src/pages/Exercicios.js
@@ -1,10 +1,14 @@
 // EcommerceHomePage.jsx
 import React from 'react'
+import getExcList from "../components/requests/Exercicio_list.js"
+
+//Teste da API wger
+const data = await getExcList();
 
 const Exercicios = () => {
   return (
     <div className="exercicios">
-        exercicios
+        {data.results[0].equipment[0].name}
     </div>
   )
 }
